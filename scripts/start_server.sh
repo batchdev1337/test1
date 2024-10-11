9ae5d719d89cff6053d2badafd32a386028e2a6b
batchdev1337 <batchdevops59@gmail.com>
2024-10-11 10:54:33 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@
 cd /home/ec2-user/my-node-app
 
 # Install dependencies (in case there are changes)
-npm install >> /root/install.log
+#npm install >> /root/install.log
 
 # Start the Node.js application
 #npm start >> /root/start.log &
